ee7c67e8d1ad4d0e41832ea725879767996981f7
Xavski <xavioursaveski1@gmail.com>
2022-07-21 00:10:12 +1000

    beginner challenges

---
+++ app.js
@@ -0,0 +1,111 @@
+
+// function addition(num1,num2){
+//     return num1 + num2
+// }
+
+// console.log(addition(7,3))
+
+
+/* */
+
+// function hourToSeconds(hours){
+//     return hours*60*60
+// }
+
+// console.log(hourToSeconds(2))
+
+/* */
+
+
+/* */
+
+// function calcPerimeter(width,length){
+//     return (2*width) + (2*length)
+// }
+ 
+// console.log(calcPerimeter(6,7))
+
+/* */
+
+
+/* */
+
+// function calcTriangleArea(base,height){
+//     return (1/2)*base*height
+// }
+// console.log(calcTriangleArea(3,2))
+
+
+/* */
+
+
+/* */
+// function appendFrontend(apple){
+//     return apple+'Frontend'
+// }
+// console.log(appendFrontend('apple'))
+/* */
+
+
+/* */
+// function sumGreaterThan100(num1,num2){
+//      return num1 + num2 >= 100
+// }
+// console.log(sumGreaterThan100(1,10))
+/* */
+
+
+/* */
+
+// function lessThanOrEqualToZero(number){
+//     return (number <= 0)
+// }
+// console.log(lessThanOrEqualToZero(0))
+
+/* */
+
+
+/* */
+// function isNotZero(number){
+//     return number === 0
+// }
+
+// console.log(isNotZero(0))
+/* */
+
+
+/* */
+// function calcRemainder(num1,num2){
+//     return num1%num2
+// }
+// console.log(calcRemainder(7,8))
+/* */
+
+
+/* */
+// function isOdd(number){
+//     return number%2 !== 0
+// }
+// console.log(isOdd(3))
+/* */
+
+
+/* */
+// function booleanInteger(int){
+//     return (int%2 === 0? 1 : -1)
+// }
+
+// console.log(booleanInteger(3))
+/* */
+
+
+
+/* */
+function isLoggedInAndSubscribed(loggedIn, subscribed){
+
+    return loggedIn === 'Logged_In' || subscribed === 'Subscribed'? true : false
+}
+console.log(isLoggedInAndSubscribed('Logged_In','Subscribed'))
+
+
+
